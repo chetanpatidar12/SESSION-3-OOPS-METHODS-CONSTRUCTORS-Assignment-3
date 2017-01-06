879095ceac2b9d7e8948d4ec11d626fec1c0ea3f
chetan patidar <chetan patidar>
2017-01-07 03:32:38 +0530

    https://github.com/chetanpatidar12/SESSION-3-Assignment-3.git
    Signed-off-by: chetan patidar <chetan patidar>

---
+++ Example.java
@@ -0,0 +1,15 @@
+package com.acad.basic;
+
+public class Example {
+	int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+	 Example pnt = new Example();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	 
+	 /**ANSWER IS option D-The program prints pnt is null 0 0.
+
+	  * **/
+	  
+}
+}
